744d70c1346b23ba726e484c0775bae94c879788
shrawan84 <58101712+shrawan84@users.noreply.github.com>
2019-12-06 11:15:43 +0000

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import os
+import crypt
+print("-------Menu---------")
+print("1, Create Directory: ")
+print("2, File Create: ")
+print("3, Check and install given software: ")
+print("4, To add/create user and password: ")
+print("5, Reboot your machine: ")
+print("6, Check current date and time: ")
+print("7, to search some thing on google: ")
+print("8, Play a song in current OS player: ")
+print("9, To search on goole to get 5 URl: ")
+print("10, Login your fb abd update a status '\"Hello Adhoc'\": ")
+print("11, To send msg '\"HELLO'\" from adhoc to whatsapp ")
+choice = int(input("Enter your choice: "))
+if(choice == 1):
+   dir_name = input("Enter directory name: ")
+   os.system("mkdir " +dir_name)
+     
+elif(choice == 2):
+  dir_name = input("Enter your file name: ")
+  os.system("touch " +dir_name)
+elif(choice == 3):
+  dir_name = input("Enter software name: ")
+  os.system("yum install -y " +dir_name)
+elif(choice == 4):
+  uname = input("Enter user_name : ")
+  password = input("Enter password : ")
+  ucrypt=crypt.crypt(password,"123")
+  os.system("useradd -m -p "+password+" "+uname)
+
+
